99955982b603201063b27c880202f5e99427b9ad
F_uu123 <421342785@qq.com>
2019-03-09 23:31:41 +0800

    feature_完成setting实体类以及Dao中的内容

---
+++ Selection_Refactor/Models/Dao/SettingDao.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Selection_Refactor.Models.Entity;
+
+namespace Selection_Refactor.Models.Dao
+{
+    public class SettingDao
+    {
+        /*
+         * Create by 付文欣  
+         * 通过年份号号获取设置
+         */
+        public Setting getById(String id)
+        {
+            SettingDBContext settingDBContext = new SettingDBContext();
+            Setting setting = settingDBContext.settings.Find(id);
+            return setting;
+        }
+
+        /*
+         * Create By 付文欣
+         * 列出所有设置
+         */
+        public List<Setting> listAll()
+        {
+            return new SettingDBContext().settings.ToList();
+        }
+
+        /*
+         * Create By 付文欣
+         * 根据年份号删除设置
+         * 成功删除返回1，失败返回0，异常返回-1
+         */
+        public int deleteById(string id)
+        {
+            try
+            {
+                Setting setting;
+                SettingDBContext settingDBContext = new SettingDBContext();
+                if ((setting = getById(id)) != null)
+                {
+                    settingDBContext.settings.Remove(setting);
+                    return settingDBContext.SaveChanges();
+                }
+                else
+                    return 0;
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                //LogUtil.writeLogToFile(e);
+                return -1;
+            }
+        }
+
+        /*
+         * Create By 付文欣
+         * 添加一个设置
+         * 成功添加返回1，失败返回0，异常返回-1
+         */
+        public int add(Setting setting)
+        {
+            try
+            {
+                SettingDBContext settingDBContext = new SettingDBContext();
+                settingDBContext.settings.Add(setting);
+                return settingDBContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                //LogUtil.writeLogToFile(e);
+                return -1;
+            }
+        }
+
+        //TODO update 函数
+
+
+        /*
+         * Create By 付文欣
+         * 根据年份号修改模式
+         * 成功添加返回1，失败返回0，异常返回-1
+         */
+        public int changemodeById(String id, int mode)
+        {
+            try
+            {
+                SettingDBContext settingDBContext = new SettingDBContext();
+                Setting setting = settingDBContext.settings.Where(m => m.yearId == id).ToList()[0];
+                settingDBContext.settings.Remove(setting);
+                setting.mode = mode;
+                settingDBContext.settings.Add(setting);
+                return settingDBContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                //LogUtil.writeLogToFile(e);
+                return -1;
+            }
+        }
+    }
+}
---
+++ Selection_Refactor/Models/Entity/Setting.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Selection_Refactor.Models.Entity
+{
+
+    public class Setting
+    {
+        
+        [Required]
+        [Key]
+        public string yearId { set; get; } // 年份ID
+
+        public DateTime infoStart { set; get; } // 填写信息开始的时间
+
+        public DateTime infoEnd { set; get; } // 填写信息结束的时间
+
+        public DateTime firstStart { set; get; } // 第一轮开始时间
+
+        public DateTime firstEnd { set; get; } // 第一轮结束时间
+
+        public DateTime secondStart { set; get; } // 第二轮开始时间
+
+        public DateTime secondEnd { set; get; } // 第二轮开始时间
+
+        public int stage { set; get; } // 阶段 取值[0,7]，mode为2时有效
+
+        public int mode { set; get; } //方式	1为按时间判断（默认） 2为手动指定
+    }
+
+    public class SettingDBContext : DbContext
+    {
+        public SettingDBContext() : base("DefaultConnection_online") { }
+
+        public DbSet<Setting> settings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Setting>().HasKey(c => new { c.yearId });
+        }
+    }
+}
