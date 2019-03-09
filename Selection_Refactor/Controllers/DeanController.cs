b67246531c262259614b8e814c4e1861e5a411be
SenierX <815118028@qq.com>
2019-03-09 09:49:45 +0800

    feature_添加教务Dean下学生列表页面Student和导师列表页面professor

---
+++ Selection_Refactor/Controllers/DeanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Selection_Refactor.Controllers
+{
+    public class DeanController : Controller
+    {
+        // GET: Dean
+        public ActionResult Professor()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+    }
+}
