7f490eb3937a5d28bdaadff1aff4bf6b36613be9
智伟 朱 <772522478@qq.com>
2019-03-17 20:06:35 +0800

    feture_添加Teacher、TeacherQuota实体类、添加TeacherDao、TeacherQuotaDao

---
+++ Selection_Refactor/Models/Dao/TeacherDao.cs
@@ -0,0 +1,152 @@
+﻿using Selection_Refactor.Models.Entity;
+using Selection_Refactor.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Selection_Refactor.Models.Dao
+{
+    public class TeacherDao
+    {
+
+        /*
+         * Create By zzw
+         * 通过教师工号获取教师
+         */
+        public Teacher getTeacherById(string id)
+        {
+            TeacherDBContext teacherDB = new TeacherDBContext();
+            Teacher teacher = teacherDB.teachers.Find(id);
+            return teacher;
+        }
+        /*
+         * Create By zzw
+         * 列出所有教师
+         * 成功插入返回1，失败返回0，异常返回-1
+         */
+        public List<Teacher> listAllTeacher()
+        {
+            TeacherDBContext teacherDB = new TeacherDBContext();
+            List<Teacher> listAll = teacherDB.teachers.ToList();
+            return listAll;
+        }
+        /*
+         * Create By zzw
+         * 插入教师
+         * 成功插入返回1，失败返回0，异常返回-1
+         */
+        public int addTeacher(Teacher teacher)
+        {
+            try
+            {
+                TeacherDBContext teacherDB = new TeacherDBContext();
+                teacherDB.teachers.Add(teacher);
+                return teacherDB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                //LogUtil.writeLogToFile(e);
+                return -1;
+            }
+        }
+        /*
+         * Create By zzw
+         * 更新教师
+         * 成功插入返回true，未查询到返回false
+         */
+        public bool updateTeacher(string id, string name, string title, string infoURL, string remark)
+        {
+            TeacherDBContext teacherDB = new TeacherDBContext();
+            List<Teacher> list = teacherDB.teachers.Where(t => t.id == id).ToList();
+            if (list.Count <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                Teacher t = list[0];
+                t.name = name;
+                t.title = title;
+                t.infoURL = infoURL;
+                t.remark = remark;
+                teacherDB.SaveChanges();
+                return true;
+            }
+        }
+        /*
+         * Create By zzw
+         * 删除教师 by id
+         * 成功插入返回1，失败返回0，异常返回-1
+         */
+        public int deleteTeacherById(string id)
+        {
+            try
+            {
+                TeacherDBContext teacherDB = new TeacherDBContext();
+                Teacher teacher = getTeacherById(id);
+                teacherDB.teachers.Remove(teacher);
+                return teacherDB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                //LogUtil.writeLogToFile(e);
+                return -1;
+            }
+        }
+        /*
+         * Create By zzw
+         * 删除教师 by ids
+         * 成功插入返回1，失败返回0，异常返回-1
+         */
+        public int deleteTeacherByIds(string[] ids)
+        {
+            List<Teacher> deleteTeachers = new List<Teacher>();
+            for (int i = 0; i < ids.Length; i++)
+            {
+                deleteTeachers.Add(getTeacherById(ids[i]));
+            }
+            try
+            {
+                TeacherDBContext teacherDB = new TeacherDBContext();
+                teacherDB.teachers.RemoveRange(deleteTeachers);
+                return teacherDB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                //LogUtil.writeLogToFile(e);
+                return -1;
+            }
+        }
+        /*
+         * Create By zzw
+         * 更改密码
+         * 成功插入返回true，失败返回false
+         */
+        public bool changePasswordById(string id, string password)
+        {
+            TeacherDBContext teacherDB = new TeacherDBContext();
+            Teacher teacher = teacherDB.teachers.Find(id);
+            if (teacher != null)
+            {
+                try
+                {
+                    teacher.password = password;
+                    teacherDB.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    //throw e;
+                    //LogUtil.writeLogToFile(e);
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Selection_Refactor/Models/Entity/Teacher.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Selection_Refactor.Models.Entity
+{
+    [Table("teacher")]
+    public class Teacher
+    {
+        [Required]
+        [Key]
+        public string id { get; set; } //教师工号
+        public string name { get; set; } //姓名
+        public string password { get; set; } //密码
+        public string title { get; set; } //职称
+        public string infoURL { get; set; } //教师简介链接
+        public string remark { get; set; } //备注信息
+    }
+    public class TeacherDBContext : DbContext
+    {
+        public TeacherDBContext() : base("DefaultConnection") { }
+        //public TeacherDBContext() : base("DefaultConnection_online") { } 
+        public DbSet<Teacher> teachers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Teacher>().HasKey(t => new { t.id }); //重写主键
+        }
+    }
+}
