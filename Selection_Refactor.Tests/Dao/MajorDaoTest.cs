f7f45543e746ce8bc6a577b14ef36f7bf49ac365
F_uu123 <421342785@qq.com>
2019-03-17 20:08:08 +0800

    feature_重新创建测试文件

---
+++ Selection_Refactor.Tests/Dao/MajorDaoTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selection_Refactor.Models.Dao;
+using Selection_Refactor.Models.Entity;
+using Selection_Refactor.Models.Validation;
+
+namespace Selection_Refactor.Tests.Dao
+{
+    [TestClass]
+    public class MajorDaoTest
+    {
+        [TestMethod]
+        public void TestMajorMethod_major()
+        {
+            MajorDao majorDao = new MajorDao();
+            Major major = new Major();
+            major.id = "a323456";
+            major.name = "英语";
+            major.remark = "123sdaSDSA";
+            majorDao.addMajor(major);
+            major = majorDao.getMajorById(major.id);
+            ValidResult validResult = ValidateHelper.IsValid(major);
+            Assert.AreEqual(1,majorDao.deleteById(major.id));
+            if (!validResult.IsVaild)
+            {
+                foreach (ErrorMember errorMember in validResult.ErrorMembers)
+                {
+                    Debug.WriteLine(errorMember.ErrorMemberName + "：" + errorMember.ErrorMessage);
+                }
+            }
+        }
+    }
+}
--- Selection_Refactor.Tests/Dao/StudentDaoTest.cs
+++ Selection_Refactor.Tests/Dao/StudentDaoTest.cs
@@ -10,26 +10,6 @@ namespace Selection_Refactor.Tests.Dao
     [TestClass]
     public class StudentDaoTest
     {
-        [TestMethod]
-        public void TestMethod()
-        {
-            MajorDao majorDao = new MajorDao();
-            Major major = new Major();
-            major.id = "a123456";
-            major.name = "英语";
-            major.remark = "123sdaSDSA";
-            majorDao.add(major);
-            major = majorDao.getById(major.id);
-            ValidResult validResult = ValidateHelper.IsValid(major);
-
-            if (!validResult.IsVaild)
-            {
-                foreach (ErrorMember errorMember in validResult.ErrorMembers)
-                {
-                    Debug.WriteLine(errorMember.ErrorMemberName + "：" + errorMember.ErrorMessage);
-                }
-            }
-
-        }
+        
     }
 }
