dd0d92da920422d48b2461322572b5acc7a33fee
RightChris <958717262@qq.com>
2019-03-24 20:34:56 +0800

    feature_create AdmnController.cs

---
+++ Selection_Refactor/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Selection_Refactor.Controllers
+{
+    public class AdminController
+    {
+    }
+}
