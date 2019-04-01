cf3b9eacb819fcedaaa9a609bf2cf6fa2b1fac5a
Unknown <1223008289@qq.com>
2019-04-01 15:30:53 +0800

    feature_完善错误处理和日志记录

--- Selection_Refactor/Attribute/ExceptionFilter.cs
+++ Selection_Refactor/Attribute/ExceptionFilter.cs
@@ -11,10 +11,10 @@ namespace Selection_Refactor.Attribute
     {
         public void OnException(ExceptionContext filterContext)
         {
-            
-                WriteLog(filterContext);
-            
-            filterContext.ExceptionHandled = true;
+            Util.LogUtil.writeLogToFile(filterContext.Exception, filterContext.RequestContext.HttpContext.Request);
+            //filterContext.ExceptionHandled = true;
+            //filterContext.RequestContext.HttpContext.Server.ClearError();
+            //filterContext.RequestContext.HttpContext.Response.Redirect("/Security/Error");
         }
 
         private Exception BuildErrorMessage(Exception ex)
@@ -25,38 +25,7 @@ namespace Selection_Refactor.Attribute
             }
             return ex;
         }
-
-        private void WriteLog(ExceptionContext filterContext)
-        {
-            var httpContext = filterContext.RequestContext.HttpContext.Request;
-            var accountCookie = filterContext.RequestContext.HttpContext.Request.Cookies["account"];
-            var ex = BuildErrorMessage(filterContext.Exception);
-            // 在出现未处理的错误时运行的代码
-            StringBuilder _builder = new StringBuilder();
-            _builder.Append("\r\n-------------  异常信息   ---------------------------------------------------------------");
-            _builder.Append("\r\n发生时间：" + DateTime.Now.ToString());
-            _builder.Append("\r\n发生异常页：" + httpContext.Url.ToString());
-            _builder.Append("\r\n异常信息：" + ex.Message);
-            _builder.Append("\r\n用户ID（cookie）：" + accountCookie ==null? accountCookie["id"]:"null");
-            _builder.Append("\r\n用户角色（cookie）：" + accountCookie == null ? accountCookie["role"] : "null");
-            _builder.Append("\r\n错误源：" + ex.Source);
-            _builder.Append("\r\n堆栈信息：" + ex.StackTrace);
-            _builder.Append("\r\n-----------------------------------------------------------------------------------------\r\n");
-            //日志物理路径
-
-            DateTime date = DateTime.Now;
-            string path = httpContext.MapPath("~/Log/");
-            string month = date.ToString("yyyy-MM");
-            if (!System.IO.Directory.Exists(path + month))
-                System.IO.Directory.CreateDirectory(path + month);
-            string currentDate = date.ToString("yyyy-MM-dd");
-            string savePath = path + month + "/" + currentDate + ".log";
-            System.IO.File.AppendAllText(savePath, _builder.ToString(), System.Text.Encoding.Default);
-            //filterContext.RequestContext.HttpContext.Server.ClearError();
-            //filterContext.ExceptionHandled = true;
-
-            //filterContext.RequestContext.HttpContext.Response.Redirect("/Home/Error");
-        }
+        
 
     }
 }
