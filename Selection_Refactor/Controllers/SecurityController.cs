ee75f88437f62fae0b0fbcce568d9ab00fddcf05
SenierX <815118028@qq.com>
2019-04-18 15:43:12 +0800

    feature_修改登录界面样式和修改密码返回信息

--- Selection_Refactor/Controllers/SecurityController.cs
+++ Selection_Refactor/Controllers/SecurityController.cs
@@ -101,7 +101,7 @@ namespace Selection_Refactor.Controllers
                         }
                         else
                         {
-                            retStr = "fail:登录失败，用户不存在或密码错误";
+                            retStr = "fail:用户不存在或密码错误";
                         }
                         return retStr;
                     case "professor":
@@ -114,7 +114,7 @@ namespace Selection_Refactor.Controllers
                         }
                         else
                         {
-                            retStr = "fail:登录失败，用户不存在或密码错误";
+                            retStr = "fail:用户不存在或密码错误";
                         }
                         return retStr;
                     case "dean":
@@ -127,7 +127,7 @@ namespace Selection_Refactor.Controllers
                         }
                         else
                         {
-                            retStr = "fail:登录失败，用户不存在或密码错误";
+                            retStr = "fail:用户不存在或密码错误";
                         }
                         return retStr;
                     case "admin":
@@ -140,11 +140,11 @@ namespace Selection_Refactor.Controllers
                         }
                         else
                         {
-                            retStr = "fail:登录失败，用户不存在或密码错误";
+                            retStr = "fail:用户不存在或密码错误";
                         }
                         return retStr;
                     default:
-                        return "fail:登录失败，没有权限";
+                        return "fail:没有权限";
                 }
             }
             catch (Exception)
