a10461d6d33901f8fb0354a6340254df52d2d8bf
chenchen zhang <762811243@qq.com>
2019-04-22 16:04:03 +0800

    feature_学生页面修改

--- Selection_Refactor/Controllers/StudentController.cs
+++ Selection_Refactor/Controllers/StudentController.cs
@@ -31,6 +31,11 @@ namespace Selection_Refactor.Controllers
             ViewBag.StuGraSchool = student.graSchool;
             ViewBag.StuGraMajor = student.graMajor;
             ViewBag.ResumeUrl = student.resumeUrl;
+            ViewBag.OnTheJob = student.onTheJob;
+
+            MajorDao majorDao = new MajorDao();
+            Major major = majorDao.getMajorById(student.majorId);
+            ViewBag.StuMajor = major.name;
             return View();
         }
         public ActionResult FinalWill()
